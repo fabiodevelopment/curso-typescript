75e5219aab4f645b5ec39d68fd2cff98206c21a1
Fabio Haddad <fabiohpaes@gmail.com>
2021-07-13 17:01:07 -0300

    [TypeScript] Modificadores de Acesso

--- classes.js
+++ classes.js
@@ -19,3 +19,31 @@ console.log(data.dia); //public
 console.log(data.mes); //private
 var data2 = new Data(1, 1);
 console.log(data2);
+var Carro = /** @class */ (function () {
+    function Carro(marca, modelo, velocidadeMax) {
+        if (velocidadeMax === void 0) { velocidadeMax = 220; }
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMax = velocidadeMax;
+        this.velocidadeAtual = 0;
+    }
+    Carro.prototype.alterarVelocidade = function (delta) {
+        if (this.velocidadeAtual + delta <= this.velocidadeMax) {
+            this.velocidadeAtual += delta;
+        }
+    };
+    Carro.prototype.acelerar = function () {
+        var aceleracao = 5;
+        this.alterarVelocidade(5);
+    };
+    Carro.prototype.frear = function () {
+        this.alterarVelocidade(-5);
+    };
+    return Carro;
+}());
+var carro = new Carro('Volkswagem', 'Gol', 250);
+// carro.velocidadeMax = 220;
+// carro.velocidadeAtual = 260;
+carro.acelerar();
+carro.frear();
+// carro.alterarVelocidade(5) // não posso chamar um metodo privado fora da classe
--- classes.ts
+++ classes.ts
@@ -17,3 +17,35 @@ console.log(data.mes); //private
 const data2 = new Data(1, 1)
 console.log(data2)
 
+class Carro {
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMax: number = 220
+    ){}
+
+    private alterarVelocidade(delta: number) {
+        if(this.velocidadeAtual + delta <= this.velocidadeMax) {
+            this.velocidadeAtual += delta;
+        }
+    }
+
+    acelerar() {
+        const aceleracao = 5;
+        this.alterarVelocidade(5)
+    }
+    frear() {
+        this.alterarVelocidade(-5)
+    }
+}
+
+const carro = new Carro('Volkswagem', 'Gol', 250)
+// carro.velocidadeMax = 220;
+// carro.velocidadeAtual = 260;
+carro.acelerar();
+carro.frear();
+
+// carro.alterarVelocidade(5) // não posso chamar um metodo privado fora da classe
+
